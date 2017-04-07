771cd10b082ccdd32cc14dea2326f86975e2ace8
youcefbarkat <barkatyoucefislam@gmail.com>
2017-04-07 16:27:14 +0200

    hoa

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var saymyname = function (name) {
+    alert('Your name is: '+name);
+}
+var car = {
+mark: 'DACIA',
+model: 'Duster',
+color: 'grey',
+isTurnedOn: false,
+numberOfWheel: 4,
+seats: [
+'seat 1',
+'seat 2',
+'seat 3',
+'seat 4',
+'seat 5'
+],
+
+turnOn: function () {
+	this.isTurnedOn = true;
+},
+fly: function () {
+	alert('fly');
+
+},
+switchcar: function (isOn) {
+	console.log("Turn car " + isOn);
+	if (isOn == true){
+		this.isTurnedOn = true;
+	}else{
+		this.isTurnedOn = false;
+	}
+}
+
+}
+
+console.log('hello there friends!');
